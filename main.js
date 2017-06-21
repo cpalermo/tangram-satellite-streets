40c80c96111e5306f344e6b22d1eb23456520975
Carlo Palermo <carlo.palermo@hdati.com>
2017-06-21 13:43:36 +0200

    Add draw controls

--- main.js
+++ main.js
@@ -9,12 +9,16 @@ var osmAttr = ' | © <a href="https://openstreetmap.org/copyright">OSM</a>' +
 var Attr = satAttr + osmAttr ;
 
 var scLayer = Tangram.leafletLayer({
-    scene: 'global.yaml',
-    attribution: Attr
+    scene: {
+	import: ['global.yaml'],
+	global: {'labels_overlay': true,
+		 'roads_overlay': true},
+    },
+    attribution: Attr,
 });
 
 var map = L.map('map' , {
-    center: [ 48.92, 11.25],
+    center: [48.92, 11.25],
     zoom: 6,
     maxZoom: 16,
     minZoom: 5,
@@ -22,13 +26,41 @@ var map = L.map('map' , {
     zoomControl: false
 });
 
-var baseLayers = {
-    "Sentinel-2": scLayer,
-};
-
 L.control.zoom({position: 'bottomright'}).addTo(map);
 
 var hash = new L.Hash(map);
+
+var sidebar = L.control.sidebar('sidebar').addTo(map);
+
+// Scene config controls
+
+var scene = scLayer.scene;
+
+document.getElementById('labels').addEventListener('click', function(){
+    var value = scene.config.global.labels_overlay;
+    if (value) {old_icon = 'fa-toggle-on'} else {old_icon = 'fa-toggle-off'}
+    if (value) {value = false} else {value = true};
+    if (value) {new_icon = 'fa-toggle-on'} else {new_icon = 'fa-toggle-off'}
+    scene.config.global.labels_overlay = value;
+    scene.updateConfig();
+    document.getElementById('labels_toggle').classList.remove(old_icon);
+    document.getElementById('labels_toggle').classList.add(new_icon);
+    //document.getElementById("test").innerHTML = value;
+    //console.log(value);
+});
+
+document.getElementById('roads').addEventListener('click', function(){
+    var value = scene.config.global.roads_overlay;
+    if (value) {old_icon = 'fa-toggle-on'} else {old_icon = 'fa-toggle-off'}
+    if (value) {value = false} else {value = true};
+    if (value) {new_icon = 'fa-toggle-on'} else {new_icon = 'fa-toggle-off'}
+    scene.config.global.roads_overlay = value;
+    scene.updateConfig();
+    document.getElementById('roads_toggle').classList.remove(old_icon);
+    document.getElementById('roads_toggle').classList.add(new_icon);
+    //document.getElementById("test").innerHTML = value;
+    //console.log(value);
+});
 
 // DEBUG
 /*
