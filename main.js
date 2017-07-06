82f7d449757bc9713a4556d952bd2c3ec68d1775
Carlo Palermo <carlo.palermo@hdati.com>
2017-07-06 17:38:22 +0200

    cleaning code

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 var satAttr = 'Modified <a href="https://scihub.copernicus.eu/">Copernicus</a>' +
     ' Sentinel data 2016 by ' +
     '<a href="https://sentinelmap.eu">SentinelMap</a>' ;
-
 var osmAttr = ' | © <a href="https://openstreetmap.org/copyright">OSM</a>' +
     ' contributors data by ' +
     '<a href="https://mapzen.com">Mapzen</a>' ;
+var attr = satAttr + osmAttr ;
 
-var Attr = satAttr + osmAttr ;
-
-var scLayer = Tangram.leafletLayer({
+var layer = Tangram.leafletLayer({
     scene: {
 	import: ['global.yaml'],
 	global: {'labels_overlay': true,
 		 'roads_overlay': true},
     },
-    attribution: Attr,
+    attribution: attr,
 });
+var scene = layer.scene;
 
 var map = L.map('map' , {
     center: [48.92, 11.25],
     zoom: 6,
     maxZoom: 16,
     minZoom: 5,
-    layers: [scLayer],
+    layers: [layer],
     zoomControl: false
 });
-
 L.control.zoom({position: 'bottomright'}).addTo(map);
 
 var hash = new L.Hash(map);
@@ -34,43 +32,41 @@ var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 // Scene config controls
 
-var scene = scLayer.scene;
-
-document.getElementById('labels').addEventListener('click', function(){
-    var value = scene.config.global.labels_overlay;
+function btn(ico_id, str_id, value) {
     if (value) {old_icon = 'fa-toggle-on'} else {old_icon = 'fa-toggle-off'}
-    if (value) {value = false} else {value = true};
-    if (value) {new_icon = 'fa-toggle-on'} else {new_icon = 'fa-toggle-off'}
-    scene.config.global.labels_overlay = value;
+    if (value) {new_icon = 'fa-toggle-off'} else {new_icon = 'fa-toggle-on'}
+    if (value) {status = 'off'} else {status = 'on'}
+    document.getElementById(ico_id).classList.remove(old_icon);
+    document.getElementById(ico_id).classList.add(new_icon);
+    document.getElementById(str_id).innerHTML = status;
+};
+
+document.getElementById('labels').addEventListener('click', function() {
+    var overlay = scene.config.global.labels_overlay;
+    btn('labels_toggle', 'labels_status', overlay);
+    if (overlay) {overlay = false} else {overlay = true};
+    scene.config.global.labels_overlay = overlay;
     scene.updateConfig();
-    document.getElementById('labels_toggle').classList.remove(old_icon);
-    document.getElementById('labels_toggle').classList.add(new_icon);
-    //document.getElementById("test").innerHTML = value;
-    //console.log(value);
 });
 
-document.getElementById('roads').addEventListener('click', function(){
-    var value = scene.config.global.roads_overlay;
-    if (value) {old_icon = 'fa-toggle-on'} else {old_icon = 'fa-toggle-off'}
-    if (value) {value = false} else {value = true};
-    if (value) {new_icon = 'fa-toggle-on'} else {new_icon = 'fa-toggle-off'}
-    scene.config.global.roads_overlay = value;
+document.getElementById('roads').addEventListener('click', function() {
+    var overlay = scene.config.global.roads_overlay;
+    btn('roads_toggle', 'roads_status', overlay);
+    if (overlay) {overlay = false} else {overlay = true};
+    scene.config.global.roads_overlay = overlay;
     scene.updateConfig();
-    document.getElementById('roads_toggle').classList.remove(old_icon);
-    document.getElementById('roads_toggle').classList.add(new_icon);
-    //document.getElementById("test").innerHTML = value;
-    //console.log(value);
 });
 
 // DEBUG
+
 /*
-scLayer.scene.subscribe({
+scene.subscribe({
     load: function (e) {
         console.log('scene loaded:', e);
     }
 });
 
-scLayer.scene.subscribe({
+scene.subscribe({
     view_complete: function () {
 	console.log('scene view complete');
     }
